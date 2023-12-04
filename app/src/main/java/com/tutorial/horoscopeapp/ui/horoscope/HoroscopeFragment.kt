ce59d4bbb128717fd075d2d3f412fad6642d8dea
ktorres63 <ktorresaro@unsa.edu.pe>
2023-12-03 20:30:37 -0500

    add HoroscopeFragment

---
+++ app/src/main/java/com/tutorial/horoscopeapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,28 @@
+package com.tutorial.horoscopeapp.ui.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tutorial.horoscopeapp.databinding.FragmentHoroscopeBinding
+
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+
+    // con !! nos aseguramos que lo q enviamos no sera null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
