9508c7be3563a1b0e17e9b4571966ddbf22ee3d1
ktorres63 <ktorresaro@unsa.edu.pe>
2023-12-05 12:46:50 -0500

    create bottomNavigationView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.tutorial.horoscopeapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tutorial.horoscopeapp"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
